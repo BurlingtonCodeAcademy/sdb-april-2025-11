f50d2dc7e1aabe0c7e61b41a648983bdf2e22a3c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-12 17:39:55 -0500

    scope example

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("HAPPY MONDAY!")
+let favoriteSnack = "poptarts"; // Global scope variable
+
+
+function classroom() {
+    let favoriteToy = "tomagachi"; // Local scope variable
+    console.log(favoriteSnack); // Accessing global variable
+    console.log("local scope", favoriteToy); // Accessing local variable
+}
+
+classroom(); // Output: poptarts
+
+console.log("global scope", favoriteToy); // Error: favoriteToy is not defined (local variable cannot be accessed outside the function)
