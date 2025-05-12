b8a91a589c7ff0a750eec4800c477346f61bb57f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-12 18:10:49 -0500

    hoist example

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ classroom(); // Output: poptarts
 // console.log("global scope", favoriteToy); // Error: favoriteToy is not defined (local variable cannot be accessed outside the function)
 
 console.log("global scope", favoriteSnack); // Output: poptarts (global variable can be accessed anywhere in the code)
+
+sayHello(); // Output: hey there!
+
+function sayHello() {
+    console.log("hey there!")
+}
+
+var name;
+
+console.log(name); // Output: undefined (hoisting: variable is declared but not initialized)
+
+name = "John"
+
+console.log(name); // Output: John (variable is now initialized)
